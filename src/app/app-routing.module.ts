178afbf0027425177bf29bd6e0de4913452fa312
Lara Lindsay <lara.lima@aec.com.br>
2023-11-24 23:12:26 -0300

    config routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,27 @@
-import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { NgModule } from '@angular/core';
+
+const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: 'auth',
+  },
+  {
+    path: 'auth',
+    loadChildren: () =>
+      import('@containers/auth/auth.module').then((m) => m.AuthModule),
+  },
+  {
+    path: 'main',
+    loadChildren: () =>
+      import('@containers/main/main.module').then((m) => m.MainModule),
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
---
+++ src/app/containers/main/main-routing.module.ts
@@ -0,0 +1,22 @@
+import { RouterModule, Routes } from '@angular/router';
+
+import { MainComponent } from '@containers/main/main.component';
+import { NgModule } from '@angular/core';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: MainComponent,
+    children: [
+      {
+        path: '',
+        pathMatch: 'full',
+        redirectTo: 'dashboard'
+      },
+      // {
+      //   path: 'dashboard',
+      //   loadChildren: () => import('@pages/dashboard/dashboard.routes').then(m => m.DashboardRoutes),
+      // },
+    ]
+  }
+]
